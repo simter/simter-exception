4689fd2ddce08664dbc3f1a15e61c8a4c5316b70
RJ.HWang <rongjihuang@gmail.com>
2018-08-13 09:19:13 +0800

    Add base SecurityException

---
+++ src/main/java/tech/simter/exception/SecurityException.java
@@ -0,0 +1,13 @@
+package tech.simter.exception;
+
+/**
+ * @author RJ
+ */
+public class SecurityException extends SimterException {
+  public SecurityException() {
+  }
+
+  public SecurityException(String message) {
+    super(message);
+  }
+}
